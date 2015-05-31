9643d019d2c0d3ebb1e3903ae566f1dae9b70cb1
William Ernest <william.lewis.ernest@gmail.com>
2015-05-30 23:23:39 -0700

    Built a logout function, that calls the devise method of logging out, and temporarily attached it to the person icon at top right.
    Changed md-content to md-dialog-content.

--- app/assets/javascripts/angular/navbar/navbar-controller.js
+++ app/assets/javascripts/angular/navbar/navbar-controller.js
@@ -1,6 +1,6 @@
 var navbar = angular.module('shiftsapp.navbar', ['ui.bootstrap', 'shiftsapp.components.auth']);
 
-navbar.controller('NavbarController', ['$scope', 'Auth', function($scope, auth) {
+navbar.controller('NavbarController', ['$scope', 'Auth', '$window', '$http', function($scope, auth, $window, $http) {
 
     var currentUserEmail = $scope.currentUserEmail;
 
@@ -13,6 +13,20 @@ navbar.controller('NavbarController', ['$scope', 'Auth', function($scope, auth) 
     $scope.isActive = function (path) {
         return path === $location.path();
     };
+
+    $scope.logout = function(){
+
+        //AJAX logout function. The response is a 302, resulting in a failure being called.
+        //TODO: Learn more about Devise and if we can use ajax requests to log the user out
+        $http.delete('/users/sign_out')
+            .then(function(){
+                //Doesn't get called.
+                $window.location.href="/";
+            }, function(){
+                //Gets called.
+                $window.location.href="/";
+            });
+    }
 
 }]);
 
