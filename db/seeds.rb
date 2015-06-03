ef86ec19c43777331214064e6807460539785f0f
William Ernest <william.lewis.ernest@gmail.com>
2015-06-03 06:18:36 -0700

    Added some seed values. Eventually I'll make a test_seed file for development that will create a lot of objects, but this works for now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(:email => "willis127@gmail.com", :password => "password", :password_confirmation => "password")
+user2 = User.create(:email => "rob.whiteside@gmail.com", :password => "password", :password_confirmation => "password")
+user.skip_confirmation!
+user2.skip_confirmation!
+user.save!
+user2.save!
+group = Group.create! :name => "Will's Group", :created_by => user.id
+group2 = Group.create! :name => "Rob's Group", :created_by => user2.id
+group.save!
+group2.save!
+shift = Shift.create! :title => "Will's Shift", :user_id => user.id, :origin => "SAN", :destination => "JFK", :group_id => group.id
+shift2 = Shift.create! :title => "Will's Shift", :user_id => user2.id, :origin => "SAN", :destination => "JFK", :group_id => group2.id
+shift.save!
+shift2.save!
