f6855d687937c4fd1591fe885b3a4a7bba18b6fa
whiteside <rob.whiteside@gmail.com>
2015-06-03 23:39:07 -0700

    Added some navigation icons to the navbar.  I call them the hanging-navbar

--- app/assets/javascripts/angular/navbar/navbar-controller.js
+++ app/assets/javascripts/angular/navbar/navbar-controller.js
@@ -2,14 +2,15 @@ var navbar = angular.module('shiftsapp.navbar', [
     'ui.bootstrap',
     'shiftsapp.components.auth']);
 
-navbar.controller('NavbarController', ['$scope', 'Auth', '$window', '$http', function($scope, auth, $window, $http) {
+navbar.controller('NavbarController', ['$scope', 'Auth', '$window', '$http', '$location', function($scope, auth, $window, $http, $location) {
 
     var currentUserEmail = $scope.currentUserEmail;
 
     //This is used to set which nav-bar item is active, but needs to be fixed
     //to use will's new directive
-    $scope.isActive = function (path) {
-        return path === $location.path();
+    $scope.classIfActive = function (path, cssClass) {
+        console.log($location.path());
+        return path === $location.path() ? cssClass : '';
     };
 
     $scope.logout = function(){
